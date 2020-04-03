4deb85bac710db9535b7af0d6534aa6f0ccd7f5a
Tim Banon <izmbanon@gmail.com>
2020-04-03 15:38:15 -0400

    Update index.php

--- api/php/www/index.php
+++ api/php/www/index.php
@@ -6,6 +6,7 @@ $data = [];
 $_GET = explode('/', $_SERVER['REQUEST_URI']); 
 array_shift($_GET); 
 $data['GET'] = $_GET;
+$data['include'] = in_array($_GET[1], ['create', 'read', 'update', 'delete']) ? '../'.$_GET[0].'/'.$_GET[1].'/index.php' : '../404.json';
 include(in_array($_GET[1], ['create', 'read', 'update', 'delete']) ? '../'.$_GET[0].'/'.$_GET[1].'/index.php' : '../404.json');
 print_r(json_encode($data, JSON_PRETTY_PRINT));
 unset($data);
